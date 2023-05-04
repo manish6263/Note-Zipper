80f600fe6e7182f3204081e99b9ab90a3a2eec48
Manish <mp626329@gmail.com>
2023-05-04 14:56:13 +0530

    server.js updated

--- server.js
+++ server.js
@@ -25,12 +25,12 @@ app.use("/api/users", userRoutes);
 
 // --------------------------deployment------------------------------
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+if (process.env.NODE_ENV === 'production') {
+  //*Set static folder up in production
+  app.use(express.static('client/build'));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+
 } else {
   app.get("/", (req, res) => {
     res.send("API is running..");
